590fe2c63dd375dab893116a7c185f2fa27282b7
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-06-06 09:03:04 +0200

    feat(column): move strategy to separate step

--- bibliography/cli.py
+++ bibliography/cli.py
@@ -15,6 +15,18 @@ DESCRIPTION = ''
 
 WORKPLAN = [
     utila.create_step(
+        'column',
+        inputs=[
+            utila.ResultFile('rawmaker', 'text_text'),
+            utila.ResultFile('rawmaker', 'text_positions'),
+            utila.ResultFile('rawmaker', 'oneline_text_text'),
+            utila.ResultFile('rawmaker', 'oneline_text_positions'),
+            utila.ResultFile('rawmaker', 'border_pages'),
+            utila.ResultFile('groupme', 'footer_footerheader'),
+        ],
+        output=('column',),
+    ),
+    utila.create_step(
         'result',
         inputs=[
             utila.ResultFile('rawmaker', 'text_text'),
--- bibliography/features/column.py
+++ bibliography/features/column.py
@@ -6,3 +6,58 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+
+import iamraw
+import serializeraw
+import utila
+
+import bibliography.layout.column
+import bibliography.utils
+
+
+def work(  # pylint:disable=R0914
+    text: str,
+    textpositions: str,
+    oneline_text: str,
+    oneline_textpositions: str,
+    sizeandborder: str,
+    headerfooter: str,
+    pages: tuple = None,
+) -> str:
+    pageslist = bibliography.utils.prepare_pages(pages)
+    parts = []
+    for selected in pageslist:
+        textnavigators = serializeraw.ptcn_fromfile(
+            text,
+            textpositions,
+            sizeandborder,
+            headerfooter,
+            pages=selected,
+        )
+        extracted = bibliography.layout.column.extracts(textnavigators)
+        utila.debug(f'column:    {bibliography.utils.count(extracted)}')
+        parts.append(extracted)
+    # select best bib ref
+    best = utila.longest(parts)
+    # remove None items
+    without_empty = [utila.notnone(page) for page in best]
+    references = utila.flatten(without_empty)
+    headline, pdfpages = None, None
+    if references:
+        pdfpages = tuple(sorted({item.raw_pdfpage for item in references}))
+        headline = bibliography.utils.search_headline(
+            oneline_text,
+            oneline_textpositions,
+            sizeandborder,
+            headerfooter,
+            page=pdfpages[0],
+        )
+    result = iamraw.BibliographyTable(
+        headline=headline,
+        references=references,
+        pdfpages=pdfpages,
+    )
+    result.__strategy__ = 'column'
+    # dump result
+    dumped = serializeraw.dump_bibliography_reference(result)
+    return dumped
